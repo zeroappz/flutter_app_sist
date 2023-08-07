ace1b7e437b611eb4f6e050ff0697917b09dd0ef
Praveen Kumar <72295783+zeroappz@users.noreply.github.com>
2023-08-07 19:36:13 +0530

    [Feat]: Dart Data Structures Programs

---
+++ dart-programming/data-structures/map.dart
@@ -0,0 +1,131 @@
+/// Map is nothing a Key:Value pair based Data Structure
+/// JSON
+main() {
+  /// Generic Map Oj=bject
+  var mapVarObj = {};
+
+  /// Type Specified Map Object
+  Map<String, dynamic> mapObject = Map();
+
+  /// Add key:value pairs
+  mapVarObj["first_name"] = "Praveen";
+  mapVarObj["last_name"] = "kumar";
+  mapVarObj["email"] = "pravileaf@gmail.com";
+  mapVarObj["phone_numbers"] = ["9047048344", "8190081187"];
+
+  /// Get value by key
+  // print(mapVarObj);
+
+  mapObject["first_name"] = "Praveen";
+  mapObject["last_name"] = "kumar";
+  mapObject["email"] = "pravileaf@gmail.com";
+  mapObject["phone_numbers"] = ["9047048344", "8190081187"];
+  mapObject["address"] = {
+    "home": {
+      "address_line_1": "ABC Street",
+      "address_line_2": "XYZ Colony",
+      "city": "Bangalore",
+      "state": "Karnataka",
+      "pin_code": 560001
+    },
+    "work": {
+      "address_line_1": "ABC Street",
+      "address_line_2": "XYZ Colony",
+      "city": "Bangalore",
+      "state": "Karnataka",
+      "pin_code": 560001
+    }
+  };
+
+  /// Accessing the mapObject
+  // print(mapObject);
+
+  /// Get value by key using anonymous function
+  mapObject.forEach((key, value) {
+    // print("${key} : ${value}");
+  });
+
+  /// Check if key exists
+  // bool isKeyExists = mapObject.containsKey("first_name");
+  bool isKeyExists = mapVarObj.containsKey("address");
+  addAddressBeforeOrder(isKeyExists, mapVarObj);
+  // print(isKeyExists);
+
+  /// Map.from()
+  // Creates map from Iterable - COnverting any object into Map
+  List<String> keys = ["first_name", "last_name", "email", "address"];
+  List<String> values = [
+    "Praveen",
+    "kumar",
+    "pravileaf@gmail.com",
+    "Bangalore"
+  ];
+  Map<String, dynamic> mapFromIterable = Map.fromIterables(keys, values);
+  print(mapFromIterable);
+  print(mapFromIterable.remove("email"));
+  print(mapFromIterable);
+
+  /// Cascading Map Objects
+  var youtubeHistory = {
+    "comment_1": {"user_id": 1, "desc": "Worst Video Explanation"}
+  };
+  var facebookHistory = {
+    "comment_2": {"user_id": 100, "desc": "Par Level Video Explanation"}
+  };
+  var instagramHistory = {
+    "comment_3": {"user_id": 102, "desc": "Great Video Explanation"}
+  };
+  Map<String, dynamic> output =
+      cascadingObjects(youtubeHistory, facebookHistory, instagramHistory);
+  if (output.isNotEmpty) {
+    print("Output os going to be printed....");
+    print(output);
+  } else {
+    print("No Data Available");
+  }
+}
+
+/// [addAddressBeforeOrder] function to check the address status
+addAddressBeforeOrder(bool keyStatus, var mapVarObj) {
+  print("inside the function");
+  if (keyStatus) {
+    print("Proceed to the checkout");
+    // print("His name is ${mapObject["first_name"]} ${mapObject["last_name"]}");
+  } else {
+    // if(keyStatus == false){
+    //   print("Proceed to the checkout");
+    // }
+    // print("Key not found");
+    print("Address not found");
+    var address = {
+      "home": {
+        "address_line_1": "ABC Street",
+        "address_line_2": "XYZ Colony",
+        "city": "Bangalore",
+        "state": "Karnataka",
+        "pin_code": 560001
+      },
+      "work": {
+        "address_line_1": "ABC Street",
+        "address_line_2": "XYZ Colony",
+        "city": "Bangalore",
+        "state": "Karnataka",
+        "pin_code": 560001
+      }
+    };
+    mapVarObj["address"] = address;
+    print("Address added");
+    // print(mapVarObj["address"]);
+    print(mapVarObj.containsKey("address"));
+
+    /// Navigating to some screen & asked you to add the address
+    addAddressBeforeOrder(mapVarObj.containsKey("address"), mapVarObj);
+  }
+}
+
+/// cascadingObjects
+Map<String, dynamic> cascadingObjects(Map<String, dynamic> obj1,
+    Map<String, dynamic> obj2, Map<String, dynamic> obj3) {
+  Map<String, dynamic> cascadedMap = {...obj1, ...obj2, ...obj3};
+  return cascadedMap;
+}
---
+++ dart-programming/data-structures/set.dart
@@ -0,0 +1,43 @@
+/// Set Object - Works with Hashing Technique
+/// Unordered objects
+/// Non-Duplicate
+main() {
+  Set<String> setObj = <String>{'Flutter', 'Python', 'Java', 'Android'};
+  // var setObj = <dynamic>{'Flutter', 'Python', 'Java', 'Android'};
+  print(setObj);
+  setObj.add('ReactJs');
+  setObj.add('Flutter');
+  setObj.addAll(['Data Science', 'AI & ML']);
+  print(setObj);
+
+  /// ElementAt
+  print(setObj.elementAt(2));
+
+  /// Hashing - Sorted Out by default
+  // late var setCount;
+  dynamic setCount;
+  if (setCount != null) {
+    print("Set has a value name ${setObj.elementAt(0)}");
+  } else {
+    /// Type Casting
+    /// Anything to String;
+    /// setCount.toString();
+
+    /// String to int Conversion
+    /// int.parse(),
+    var obj = int.parse('1');
+    print(obj.runtimeType);
+
+    /// String to int Conversion
+    /// int.parse(),
+    var objDouble = double.parse('1');
+    print(objDouble.runtimeType);
+
+    Set _result;
+    List objList = ['Praveen', 'Ramu', 'Tanya', 'Vamsi'];
+    _result = objList.toSet();
+    print(_result.runtimeType);
+
+    print("Set has a value name ${setObj.elementAt(0)}");
+  }
+}
